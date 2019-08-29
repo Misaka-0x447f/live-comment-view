bef1598123de2e7e013ec578e9cf0fc67d0cec4e
Misaka <447f.misaka@outlook.com>
2019-08-29 18:30:02 +0800

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: "ng-root",
+  template: `
+      <ng-host></ng-host>`,
+  styles: [],
+})
+export class AppComponent {
+}
---
+++ src/app/host/host.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "ng-host",
+  templateUrl: "./host.component.html",
+  styleUrls: ["./host.component.styl"]
+})
+export class HostComponent implements OnInit {
+
+  constructor() { }
+
+  public ngOnInit() {
+  }
+
+}
