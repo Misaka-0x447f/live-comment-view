9ceef7832b39c8dcb0f8302e51cef069d8793ef1
Misaka <447f.misaka@outlook.com>
2019-09-04 13:48:48 +0800

    done member message

--- src/interface/networks/watermelon/conv/memberMessage.ts
+++ src/interface/networks/watermelon/conv/memberMessage.ts
@@ -5,15 +5,20 @@ import i18n from "../../../../utils/i18n";
 
 export const toMemberMessage = (d: unknown) => {
   const type = get(d, "extra.action");
-  const user = toUser(d);
+  const u = toUser(d).name;
   return {
     type,
-    typeEnum: (() => {
+    plainText: (() => {
       selectCase({
         exp: type,
         case: [
-          [1, () => i18n.comments.inbound(user.name)],
+          [1, () => i18n.comments.inbound(u)],
+          [3, () => i18n.comments.banned(u)],
+          [4, () => i18n.comments.unbanned(u)],
+          [5, () => i18n.comments.elevated(u)],
+          [12, () => i18n.comments.subscribed(u)],
         ],
+        def: () => !i18n.comments.notRegistered(u, type),
       });
     })(),
     content: get(d, "extra.content"),
