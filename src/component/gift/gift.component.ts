833b760a7b6f71efc3756ad76dc9b1239d446d3b
Misaka <447f.misaka@outlook.com>
2019-10-07 17:06:21 +0800

    feat: gift animation

--- src/component/gift/gift.component.ts
+++ src/component/gift/gift.component.ts
@@ -1,12 +1,27 @@
 import {Component, Input, OnInit} from "@angular/core";
 import {Watermelon} from "../../interface/networks/watermelon/interfaces/watermelon";
 import i18n from "../../utils/i18n";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: "ng-gift",
   templateUrl: "./gift.component.html",
   styleUrls: ["./gift.component.styl"],
+  animations: [
+    trigger("new", [
+      state("new", style({
+        background: "rgba(192,255,0,0)",
+      })),
+      transition(":enter", [
+        style({
+          background: "rgba(192,255,0,0.6)",
+        }),
+        animate(1000),
+      ]),
+    ]),
+  ],
 })
+
 export class GiftComponent implements OnInit {
   @Input() private worker: Watermelon;
 
