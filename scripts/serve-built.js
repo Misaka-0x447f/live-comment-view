c19acddaeaf892d908af8f5ce0273a1cc7919586
MisakaKumomi <447f.misaka@outlook.com>
2020-01-25 15:48:13 +0800

    feat: auto update

--- scripts/serve-built.js
+++ scripts/serve-built.js
@@ -7,62 +7,74 @@ import moment from 'moment';
 import express from 'express';
 import execa from "execa";
 
-const allowedExt = [
-  '.js',
-  '.ico',
-  '.css',
-  '.png',
-  '.jpg',
-  '.woff2',
-  '.woff',
-  '.ttf',
-  '.svg',
-];
+import git from "simple-git";
 
-const dir = path.resolve("./dist/live-comment-view/");
-const port = 4200;
+// check updates
+console.log("Checking updates...")
+git("./").pull(async (...args) => {
+  if (args[1].files.length) {
+    console.log("Got some updates. Now building fresh packs...");
+    await execa("yarn", [], {stdout: "inherit", stderr: "inherit"});
+    await execa("yarn", ["start"], {stdout: "inherit", stderr: "inherit"});
+  } else {
+    const allowedExt = [
+      '.js',
+      '.ico',
+      '.css',
+      '.png',
+      '.jpg',
+      '.woff2',
+      '.woff',
+      '.ttf',
+      '.svg',
+    ];
 
-class Server {
-  app;
+    const dir = path.resolve("./dist/live-comment-view/");
+    const port = 4200;
 
-  constructor() {
-    this.app = express();
+    class Server {
+      app;
 
-    // Redirect all the other requests
-    this.app.get('*', (req, res) => {
-      if (allowedExt.filter(ext => req.url.indexOf(ext) > 0).length > 0) {
-        res.sendFile(path.resolve(dir, `./${req.url}`));
-      } else {
-        res.sendFile(path.resolve(dir, './index.html'));
+      constructor() {
+        this.app = express();
+
+        // Redirect all the other requests
+        this.app.get('*', (req, res) => {
+          if (allowedExt.filter(ext => req.url.indexOf(ext) > 0).length > 0) {
+            res.sendFile(path.resolve(dir, `./${req.url}`));
+          } else {
+            res.sendFile(path.resolve(dir, './index.html'));
+          }
+        });
+
+        // Depending on your own needs, this can be extended
+        this.app.use(bodyParser.json({limit: '50mb'}));
+        this.app.use(bodyParser.raw({limit: '50mb'}));
+        this.app.use(bodyParser.text({limit: '50mb'}));
+        this.app.use(bodyParser.urlencoded({
+          limit: '50mb',
+          extended: true
+        }));
+
+        // Start the server on the provided port
+        this.app.listen(port, () => console.log(`** server started at port ${port} **`));
+
+        // Catch errors
+        this.app.on('error', (error) => {
+          console.error(moment().format(), 'ERROR', error);
+        });
+
+        process.on('uncaughtException', (error) => {
+          console.log(moment().format(), error);
+        });
       }
-    });
 
-    // Depending on your own needs, this can be extended
-    this.app.use(bodyParser.json({limit: '50mb'}));
-    this.app.use(bodyParser.raw({limit: '50mb'}));
-    this.app.use(bodyParser.text({limit: '50mb'}));
-    this.app.use(bodyParser.urlencoded({
-      limit: '50mb',
-      extended: true
-    }));
+      static bootstrap() {
+        return new Server();
+      }
+    }
 
-    // Start the server on the provided port
-    this.app.listen(port, () => console.log(`** server started at port ${port} **`));
-
-    // Catch errors
-    this.app.on('error', (error) => {
-      console.error(moment().format(), 'ERROR', error);
-    });
-
-    process.on('uncaughtException', (error) => {
-      console.log(moment().format(), error);
-    });
+    execa('node', ['--experimental-modules', './cors-side.js'], {stdout: "inherit", stderr: "inherit"});
+    Server.bootstrap();
   }
-
-  static bootstrap() {
-    return new Server();
-  }
-}
-
-execa('node', ['--experimental-modules', './cors-side.js'], {stdout: "inherit", stderr: "inherit"});
-Server.bootstrap();
+})
